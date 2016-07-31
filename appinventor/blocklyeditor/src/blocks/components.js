f2086bbfe328cdb583a998ea8097581a8934716e
Emily Giurleo <egiurleo@mit.edu>
2016-07-31 13:04:36 +0200

    fixed other merge issues

--- appinventor/blocklyeditor/src/blocks/components.js
+++ appinventor/blocklyeditor/src/blocks/components.js
@@ -619,7 +619,6 @@ Blockly.Blocks.component_method = {
     return tb;
   },
 
-<<<<<<< HEAD
   addEmptyInput : function () {
     // Nothing here
   },
@@ -691,7 +690,7 @@ Blockly.Blocks.component_method = {
       return Blockly.decompose(workspace, 'js_input', this);
     } else if (this.methodName == "CreateJavaScriptObject") {
       return Blockly.decompose(workspace, 'js_attribute', this);
-=======
+    }
   // check if block corresponds to a method inside componentTypes[typeName].methodDictionary
   verify : function() {
 
@@ -764,7 +763,6 @@ Blockly.Blocks.component_method = {
       }
     } else {
       this.badBlock();
->>>>>>> master
     }
   }
 
