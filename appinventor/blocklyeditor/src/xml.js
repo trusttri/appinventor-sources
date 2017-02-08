fbd2dd0003a86f86931e2d1fcc2de2ace2f690ae
Evan W. Patton <ewpatton@mit.edu>
2017-02-08 17:07:14 -0500

    Remove workspace render state in domToWorkspaceHeadless
    Change-Id: I5812047e184282175209c737975421c215725c5c

--- appinventor/blocklyeditor/src/xml.js
+++ appinventor/blocklyeditor/src/xml.js
@@ -78,7 +78,6 @@ Blockly.Xml.domToWorkspace = (function(func) {
     return func;
   } else {
     var f = function() {
-      var savedRendered = arguments[1].rendered;
       var args = Array.prototype.slice.call(arguments);
       Blockly.Instrument.timer (
         function() {
@@ -88,7 +87,6 @@ Blockly.Xml.domToWorkspace = (function(func) {
           Blockly.Instrument.stats.domToWorkspaceCalls++;
           Blockly.Instrument.stats.domToWorkspaceTime = timeDiff;
         });
-      arguments[1].rendered = savedRendered;
     };
     f.isInstrumented = true;
     return f;
